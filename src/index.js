b310e676e49c642385574641e826620441150f12
Максим Самойлов <samoilowmaxim@gmail.com>
2016-01-09 23:54:29 +0600

    refactor: rewrite without nesting

--- src/index.js
+++ src/index.js
@@ -3,31 +3,39 @@ import {
  returnStatement,
 } from 'babel-types'
 
+
+function expressionVisitor(path) {
+  if (this.returnAdded) return
+  // path.replaceWith(returnStatement(path.node))
+  path.replaceWith(awaitExpression(path.node))
+  this.returnAdded = true
+}
+
+function expressionStatementVisitor(path) {
+  if (this.returnAdded || path.node !== this.latestNodeInBody) return
+  path.traverse({
+    Expression: expressionVisitor,
+  }, { returnAdded: this.returnAdded })
+}
+
+function blockStatementVisitor(path) {
+  const functionBody = path.node.body
+  const latestNodeInBody = functionBody[functionBody.length - 1]
+  path.traverse({
+    ExpressionStatement: expressionStatementVisitor,
+  }, { latestNodeInBody, returnAdded: false })
+}
+
+function functionDeclarationVisitor(path) {
+  path.traverse({
+    BlockStatement: blockStatementVisitor,
+  })
+}
+
 export default function({ types: t }) {
   return {
     visitor: {
-      FunctionDeclaration(path) {
-        path.traverse({
-          BlockStatement(path) {
-            const functionBody = path.node.body
-            const start = functionBody[functionBody.length - 1]
-            let stop = null;
-            path.traverse({
-              ExpressionStatement(path) {
-                if (path.node !== start) return
-                path.traverse({
-                  Expression(path) {
-                    if (stop) return
-                    stop = true
-                    // path.replaceWith(returnStatement(path.node))
-                    path.replaceWith(awaitExpression(path.node))
-                  }
-                })
-              }
-            })
-          }
-        })
-      }
+      FunctionDeclaration: functionDeclarationVisitor,
     }
   }
 }
