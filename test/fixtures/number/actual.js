ab5fdb18db78fe166a7139ffefc6aada884794ee
Максим Самойлов <samoilowmaxim@gmail.com>
2016-01-09 19:49:02 +0600

    Initial commit

---
+++ test/fixtures/number/actual.js
@@ -0,0 +1,3 @@
+function getOne() {
+	1
+}
---
+++ test/test.js
@@ -0,0 +1,22 @@
+require('chai').should()
+import fs from 'fs'
+import path from 'path'
+import { transformFileSync } from 'babel-core'
+
+
+describe('Add return', () => {
+  const fixturesDir = path.join(__dirname, 'fixtures')
+  fs.readdirSync(fixturesDir).map(caseName => {
+    it(caseName, function() {
+      const fixtureDir = path.join(fixturesDir, caseName)
+      const actualPath = path.join(fixtureDir, 'actual.js')
+      const actual = transformFileSync(actualPath).code
+
+      const expected = fs.readFileSync(
+        path.join(fixtureDir, 'expected.js')
+      ).toString()
+
+      actual.trim().should.be.equal(expected.trim())
+    })
+  })
+})
